0208c0c23a0bb4a61f0808fedf5d5c459484f3b4
alexander flores <alexfloresrayme@gmail.com>
2025-11-10 18:01:20 -0500

    feat(auth): refactor AuthGuard to use dynamic cookie secret for token retrieval

--- apps/api/src/guards/auth.guard.ts
+++ apps/api/src/guards/auth.guard.ts
@@ -4,17 +4,23 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { JwtService } from '@nestjs/jwt';
+import { ConfigType } from '../config/env';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService<ConfigType>,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const ctx = GqlExecutionContext.create(context);
     const { req } = ctx.getContext();
-    const token = req.cookies['auth_token'];
+    const authCookieName = this.configService.get('COOKIE_SECRET');
+    const token = req.cookies[authCookieName];
 
     if (!token) {
       throw new UnauthorizedException('No authentication token found');
